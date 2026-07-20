c09b4f5f1d65f0667ddae39fa067705d68a80247
unknown <varshahanumakonda@gmail.com>
2026-07-20 19:36:59 +0530

    Initial commit

---
+++ Day1_variables_datatypes/ex.py
@@ -0,0 +1,15 @@
+name='varsha'
+print(name)
+_name='Varsha'
+print(_name)
+NAME='VARSHA'
+print(NAME)
+Name='varsha'
+print(Name)
+
+#datatypes
+a=20
+print(bin(a))
+print(oct(a))
+print(hex(a))
+print(int(a))
